a75ed728f864d9854a4bbc744b5a74897b8600ba
unaoya <umezakinaoya@gmail.com>
2018-04-03 08:53:43 +0900

    revise

--- presheaf.v
+++ presheaf.v
@@ -1,14 +1,14 @@
 Definition Subset (X : Set) : Type := X -> Prop.
 Definition empty (X : Set) : Subset X := fun (x : X) => x <> x.
-Definition intsec {X : Set} (S1 S2 : Subset X) : Subset X :=
-  fun (x : X) => S1 x /\ S2 x.
 Inductive incl {X : Set} : Subset X -> Subset X -> Prop :=
 | Incl_intro : forall U V : Subset X,
     (forall x : X, U x -> V x) -> incl U V.
-Variable X : Set.
+Definition intsec {X : Set} (U V : Subset X) : Subset X :=
+  fun (x : X) => U x /\ V x.
+Definition fiber {X A : Set} (f : A -> Subset X) (V : Subset X) : Subset A :=
+  fun (a : A) => V = (f a).
 
-Hypothesis subset_equal : forall U V : Subset X, (forall x : X, U x <-> V x) -> U = V.  
-
+Hypothesis subset_equal : forall {X : Set} (U V : Subset X), (forall x : X, U x <-> V x) -> U = V.  
 Lemma incl_cap : forall {X : Set} (U V : Subset X) (x : X), incl U V -> (intsec U V) x <-> U x.
   Proof.
   intros.
@@ -23,50 +23,34 @@ apply H0.
 apply H.
 apply H0.
   Qed.
-  
-Variable A : Set.
-Variable E : A -> Subset X.
-Variable r : A -> Subset X -> A.
 
-Hypothesis A_eq : forall a b : A,  a = b -> b = a.
-Hypothesis A_assoc : forall a b c : A, a = b /\ b = c -> a = c.
-Hypothesis subset_eq : forall U V : Subset X, U = V -> forall a : A, r a U = r a V.
-Hypothesis ps_empty : forall a b : A, r a (empty X) = r b (empty X).
-Hypothesis ps_res_idempotent : forall a : A, r a (E a) = a.
+  Lemma incl_trans : forall {X : Set} (U V W : Subset X), incl U V -> incl V W -> incl U W.
+  Proof.
+    intros.
+destruct H.
+destruct H0.
+apply Incl_intro.
+intros.
+apply H0.
+apply H.
+apply H1.
+  Qed.
 
-Lemma res : forall a b : A, forall U : Subset X, E a = U -> r b (E a) = r b U.
-Proof.
-  intros.
-  apply subset_eq.
-  apply H.
-Qed.
-
-Lemma empty_idemp_lem : forall a : A, E a = empty X -> r a (empty X) = a.
-  intros.
-  apply A_eq.
-  rewrite <- H.
-  apply A_eq.
-  apply ps_res_idempotent.
-Qed.
-
-Lemma empty_idemp : forall a b : A, ((E a) = (empty X)) -> ((E b) = (empty X)) -> a = b.
-  intros.
-apply empty_idemp_lem in H.
-apply empty_idemp_lem in H0.
-rewrite <- H.
-rewrite <- H0.
-apply ps_empty.
-Qed.  
-
-Hypothesis comm_e_res : forall a : A, forall U : Subset X, E (r a U) = intsec (E a) U.
-Hypothesis res_assoc : forall a : A, forall U V : Subset X, r (r a U) V = r a (intsec U V).
-Lemma res_id : forall a : A, forall U : Subset X, E a = U -> r a U = a.
-  intros.
-  rewrite <- H.
-  apply ps_res_idempotent.
-Qed.
-
-Lemma inclusion : forall x : X, forall U V : Subset X, incl U V -> U x -> V x.
+  Lemma intsec_comm : forall {X : Set} (U V : Subset X), intsec U V = intsec V U.
+  Proof.
+    intros.
+    unfold intsec.
+    apply subset_equal.
+    intros.
+    split.
+    split.
+    apply H.
+    apply H.
+    split.
+    apply H.
+    apply H.
+    Qed.
+Lemma inclusion : forall {X : Set} (U V : Subset X) (x : X), incl U V -> U x -> V x.
 Proof.
   intros.
   destruct H.
@@ -74,7 +58,7 @@ apply H.
 apply H0.
 Qed.
 
-Lemma intsec_and : forall U V : Subset X, forall x : X, (intsec U V) x <-> U x /\ V x.
+Lemma intsec_and : forall {X : Set} (U V : Subset X) (x : X), (intsec U V) x <-> U x /\ V x.
 Proof.
   intros.
   split.
@@ -85,7 +69,7 @@ Proof.
   apply H.
   apply H.
 Qed.
-Lemma incl_intsec : forall U V : Subset X, forall x : X, incl U V -> (intsec U V) x <->  U x.
+Lemma incl_intsec : forall {X : Set} (U V : Subset X) (x : X), incl U V -> (intsec U V) x <->  U x.
 Proof.
   intros.
   unfold intsec.
@@ -100,11 +84,63 @@ Proof.
   apply H0.
 Qed.
 
-Lemma res_incl : forall a : A, forall U V : Subset X, incl U V -> E a = V -> r (r a V) U = r a U.
+Lemma fiber_in : forall {X A : Set} (U : Subset X) (a : A) (f : A -> Subset X),
+        fiber f U a -> U = (f a).
+Proof.
+  intros.
+destruct H.
+reflexivity.
+Qed.
+
+(* Definition of presheaf *)
+
+Variable X A : Set.
+Variable E : A -> Subset X.
+Variable r : A -> Subset X -> A.
+Hypothesis A_eq : forall a b : A,  a = b -> b = a.
+Hypothesis A_assoc : forall a b c : A, a = b /\ b = c -> a = c.
+
+Hypothesis ps_empty : forall a b : A, r a (empty X) = r b (empty X).
+Hypothesis r_intsec : forall a : A, forall U V : Subset X, r (r a U) V = r a (intsec U V).
+Hypothesis E_domain : forall a : A, r a (E a) = a.
+Hypothesis E_r : forall a : A, forall U : Subset X, E (r a U) = intsec (E a) U.
+
+Lemma res : forall a b : A, forall U : Subset X, E a = U -> r b (E a) = r b U.
+Proof.  
+  intros.
+f_equal.
+  apply H.
+Qed.
+
+Lemma empty_idemp_lem : forall a : A, E a = empty X -> r a (empty X) = a.
+  Proof.
+  intros.
+  apply A_eq.
+  rewrite <- H.
+  apply A_eq.
+  apply E_domain.
+Qed.  
+
+Lemma empty_idemp : forall a b : A, ((E a) = (empty X)) -> ((E b) = (empty X)) -> a = b.
+  intros.
+apply empty_idemp_lem in H.
+apply empty_idemp_lem in H0.
+rewrite <- H.
+rewrite <- H0.
+apply ps_empty.
+Qed.  
+
+Lemma res_id : forall (a : A) (U : Subset X), E a = U -> r a U = a.
+  intros.
+  rewrite <- H.
+  apply E_domain.
+Qed.
+
+Lemma res_incl : forall (a : A) (U V : Subset X), incl U V -> E a = V -> r (r a V) U = r a U.
 Proof.
   intros.  
   assert (r (r a V) U = r a (intsec V U)).
-  apply res_assoc.
+  apply r_intsec.
   assert (intsec V U = U).
   apply subset_equal.
   intro.
@@ -120,13 +156,61 @@ Proof.
 apply H2.
 apply H2.
 assert (r a (intsec V U) = r a U).
-apply subset_eq.
+f_equal.
 apply H2.
 rewrite <- H3.
   apply H1.
 Qed.
 
+Lemma r_id : forall (U : Subset X) (a : A), fiber E U a -> r a U = a.
+Proof.
+  intros.
+  assert (U = (E a)).
+  apply fiber_in.
+  apply H.
+  assert (r a U = r a (E a)).
+  f_equal.
+  apply H0.
+  rewrite H1.
+  apply E_domain.
+Qed.
 
+
+  Lemma r_trans : forall (U V W : Subset X) (a : A), incl U V -> incl V W -> r (r a V) U = r a U.
+  Proof.
+        intros.    
+        assert (intsec U V = U).
+        unfold intsec.
+        apply subset_equal.
+        intros.
+        split.
+        intros.
+        apply H1.
+        intros.
+        split.
+        apply H1.
+        destruct H.
+        apply H.
+        apply H1.
+        assert (r (r a V) U = r a (intsec V U)).
+        apply r_intsec.
+    rewrite H2.
+    f_equal.
+    unfold intsec.
+    apply subset_equal.
+    intros.
+    split.
+    intro.
+    apply H3.
+    intros.
+    split.
+    destruct H.
+    apply H.
+    apply H3.
+    apply H3.    
+  Qed.
+
+  
   
 
                                                                              
